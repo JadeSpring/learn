a2614d3ca9bd2baa32dbfe2eaaf87fa997687f95
邵玉春 <yuchun.shao@ly.com>
2021-03-11 19:29:47 +0800

    hh

--- src/main/java/syc/learn/designpattern/factory/App.java
+++ src/main/java/syc/learn/designpattern/factory/App.java
@@ -8,7 +8,7 @@ import java.net.URLEncoder;
  */
 public class App {
     public static void main(String[] args) throws Exception {
-        String req = "{\"ip\":\"" + "218.195.219.255"
+        String req = "{\"ip\":\"" + "123.77.0.2"
                 + "\",\"callback\":\"\",\"acc\":\"2e538071f20148a998ad91ebdda56303\",\"method\":\"getIpLoc\"}";
         System.out.println(URLEncoder.encode(req, "utf-8"));
     }
---
+++ src/main/java/syc/learn/jvm/classloading/ClassloadingApp.java
@@ -0,0 +1,12 @@
+package syc.learn.jvm.classloading;
+
+/**
+ * @author yuchun.shao
+ * @date 2020/4/17
+ */
+public class ClassloadingApp {
+    public static void main(String[] args) {
+//        System.out.println(SuperClass.class.getName());
+        System.out.println(SuperClass.WORLD);
+    }
+}
